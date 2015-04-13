3ed4d30f1a2f2f962d993b0701e4542266334af9
Michael Kutý <6du1ro.n@gmail.com>
2015-04-13 23:37:45 +0200

    load widget themes as management command

---
+++ leonardo/module/web/management/commands/sync_themes.py
@@ -0,0 +1,74 @@
+import codecs
+import os
+from optparse import make_option
+
+from django.core.management.base import CommandError, NoArgsCommand
+from leonardo.module.web.models import Widget, WidgetTheme
+
+from .utils import get_or_create_template
+
+"widget.verbose_name - template.name"
+THEME_NAME_FORMAT = "{0} {1}"
+
+
+class Command(NoArgsCommand):
+    help = "Syncs file system templates and themes with the database bidirectionally.\
+    based on dbtemplates.sync_templates"
+    option_list = NoArgsCommand.option_list + (
+        make_option("-e", "--ext",
+                    dest="ext", action="store", default="html",
+                    help="extension of the files you want to "
+                         "sync with the database [default: %default]"),
+        make_option("-f", "--force",
+                    action="store_true", dest="force", default=False,
+                    help="overwrite existing database templates"),
+        make_option("-o", "--overwrite",
+                    action="store", dest="overwrite", default='0',
+                    help="'0' - ask always, '1' - overwrite database "
+                         "templates from template files, '2' - overwrite "
+                         "template files from database templates"),
+        make_option("-a", "--app-first",
+                    action="store_true", dest="app_first", default=False,
+                    help="look for templates in applications "
+                         "directories before project templates"),
+        make_option("-d", "--delete",
+                    action="store_true", dest="delete", default=False,
+                    help="Delete templates after syncing"))
+
+    def handle_noargs(self, **options):
+        extension = options.get('ext')
+        force = options.get('force')
+        overwrite = options.get('overwrite')
+        app_first = options.get('app_first')
+        delete = options.get('delete')
+
+        # TODO: base template as param
+        base_template_name = 'widget/base.html'
+        base_template = get_or_create_template(base_template_name)
+
+        created_themes = 0
+
+        for w in Widget.__subclasses__():
+            templates = w.templates()
+            for name in templates:
+
+                widget_template = get_or_create_template(name)
+
+                if not widget_template:
+                    self.stdout.write('Template for "%s" not found' % name)
+                    continue
+
+                try:
+                    widget_theme = WidgetTheme.objects.get(
+                        content_template__name__exact=name)
+                except WidgetTheme.DoesNotExist:
+                    widget_theme = WidgetTheme()
+                    widget_theme.label = THEME_NAME_FORMAT.format(
+                        unicode(w._meta.verbose_name), name.split("/")[-1])
+                    widget_theme.content_template = widget_template
+                    widget_theme.base_template = base_template
+                    widget_theme.widget_class = w.__class__.__name__
+                    widget_theme.save()
+                    created_themes += 1
+
+        self.stdout.write('Successfully created %s widget themes' % created_themes)
---
+++ leonardo/module/web/management/commands/utils.py
@@ -0,0 +1,44 @@
+import codecs
+import os
+
+from dbtemplates.conf import settings
+from dbtemplates.models import Template
+from django.contrib.sites.models import Site
+from django.template.loaders.app_directories import app_template_dirs
+
+ALWAYS_ASK, FILES_TO_DATABASE, DATABASE_TO_FILES = ('0', '1', '2')
+
+
+def get_or_create_template(template_name, extension='.html', app_first=False,
+                           force=True, overwrite='1'):
+
+    site = Site.objects.get_current()
+
+    if not extension.startswith("."):
+        extension = ".%s" % extension
+
+    if app_first:
+        tpl_dirs = app_template_dirs + settings.TEMPLATE_DIRS
+    else:
+        tpl_dirs = settings.TEMPLATE_DIRS + app_template_dirs
+    templatedirs = [d for d in tpl_dirs if os.path.isdir(d)]
+
+    for templatedir in templatedirs:
+        for dirpath, subdirs, filenames in os.walk(templatedir):
+            for f in [f for f in filenames
+                      if f.endswith(extension) and not f.startswith(".")]:
+                path = os.path.join(dirpath, f)
+                name = path.split(templatedir)[1]
+                if name.startswith('/'):
+                    name = name[1:]
+                if template_name in name:
+                    try:
+                        t = Template.on_site.get(name__exact=name)
+                    except Template.DoesNotExist:
+                        t = Template(name=name,
+                                     content=codecs.open(path, "r").read())
+                        t.save()
+                        t.sites.add(site)
+
+                    return t
+    return None
