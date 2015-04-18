94f299a5a1875fda4b85c89e7569eaa4b4f62b03
Ales Komarek <mail@newt.cz>
2015-04-18 19:06:30 +0200

    navigation

--- leonardo/module/nav/widget/sitemap/models.py
+++ leonardo/module/nav/widget/sitemap/models.py
@@ -18,12 +18,12 @@ class SiteMapWidget(Widget):
         request = options['request']
         lang = get_language_from_request(request)
         page_list = Page.objects.filter(level=0, active=True, language=lang)
-        linkmenu_class = models.loading.get_model('page', 'linkmenuwidget')
-        utils_list = linkmenu_class.objects.filter(parent__in=page_list)
+#        linkmenu_class = models.loading.get_model('page', 'linkmenuwidget')
+#        utils_list = linkmenu_class.objects.filter(parent__in=page_list)
 
         return render_to_string(self.get_template_name(format='xml'), { 
             'widget': self,
             'page_list': page_list,
-            'utils_list': utils_list,
+#            'utils_list': utils_list,
             'request': request,
         })
