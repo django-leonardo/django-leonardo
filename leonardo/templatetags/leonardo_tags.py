56ec2508018f38b6d66c715c0f32f14c12021de9
Michael Kutý <6du1ro.n@gmail.com>
2015-04-18 20:30:58 +0200

    fix

--- leonardo/templatetags/leonardo_tags.py
+++ leonardo/templatetags/leonardo_tags.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.simple_tag
 def get_col_classes(page, region):
     data = page.get_col_classes(region)
     if str(data) == "":
