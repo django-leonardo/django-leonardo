7b1b70398eb0423dcf685f3828dca59c72c2872b
Michael Kutý <6du1ro.n@gmail.com>
2015-04-19 13:27:03 +0200

    headings and item_editor css from emma

--- leonardo/site.py
+++ leonardo/site.py
@@ -1,9 +1,14 @@
 
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
 
 
 class LeonardoAdminSite(AdminSite):
+
+    site_header = getattr(settings, "SITE_HEADER", "Leonardo administration")
+
+    site_title = getattr(settings, "SITE_TITLE", "Leonardo site admin")
 
     def get_urls(self):
 
