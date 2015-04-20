0a1351901eb7fbc70e37a6b7b29ada3de1a9ccd3
Michael Kutý <6du1ro.n@gmail.com>
2015-04-20 11:45:12 +0200

    themes as packages initial implementation

--- leonardo/module/web/__init__.py
+++ leonardo/module/web/__init__.py
@@ -1,10 +1,13 @@
 
+import logging
 from django.apps import AppConfig
 
 from .widget import *
 
 
 default_app_config = 'leonardo.module.web.WebConfig'
+
+LOG = logging.getLogger(__name__)
 
 
 class Default(object):
@@ -18,8 +21,23 @@ class Default(object):
         ]
 
     @property
+    def themes(self):
+        """supported themes
+        """
+        return ["leonardo_theme_adminlte", ]
+
+    @property
     def apps(self):
-        return [
+
+        INSTALLED_APPS = []
+        for theme in self.themes:
+            try:
+                __import__(theme)
+                INSTALLED_APPS += [theme]
+            except ImportError:
+                LOG.warning("you are missing available theme {}".format(theme))
+
+        return INSTALLED_APPS + [
             'feincms',
             'mptt',
             'crispy_forms',
