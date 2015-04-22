7db880bb2c5bfaf44ead1cc14850e35e4c16da21
Michael Kutý <6du1ro.n@gmail.com>
2015-04-22 08:59:03 +0200

    xstatic fixes

--- leonardo/conf/static.py
+++ leonardo/conf/static.py
@@ -14,6 +14,9 @@ COMPRESS_PARSER = 'compressor.parser.HtmlParser'
 
 import xstatic.main
 import xstatic.pkg.angular
+import xstatic.pkg.angular_bootstrap
+import xstatic.pkg.angular_lrdragndrop
+import xstatic.pkg.angular_smart_table
 import xstatic.pkg.bootstrap_datepicker
 import xstatic.pkg.bootstrap_scss
 import xstatic.pkg.d3
@@ -26,43 +29,75 @@ import xstatic.pkg.jquery_quicksearch
 import xstatic.pkg.jquery_tablesorter
 import xstatic.pkg.jquery_ui
 import xstatic.pkg.jsencrypt
+import xstatic.pkg.magic_search
 import xstatic.pkg.qunit
 import xstatic.pkg.rickshaw
 import xstatic.pkg.spin
+import xstatic.pkg.termjs
 import xstatic.pkg.adminlte
 import xstatic.pkg.react
 
 STATICFILES_DIRS = [
     ('horizon/lib/angular',
-        xstatic.main.XStatic(xstatic.pkg.angular).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.angular,
+                             root_url=webroot).base_dir),
+    ('horizon/lib/angular',
+        xstatic.main.XStatic(xstatic.pkg.angular_bootstrap,
+                             root_url=webroot).base_dir),
+    ('horizon/lib/angular',
+        xstatic.main.XStatic(xstatic.pkg.angular_lrdragndrop,
+                             root_url=webroot).base_dir),
+    ('horizon/lib/angular',
+        xstatic.main.XStatic(xstatic.pkg.angular_smart_table,
+                             root_url=webroot).base_dir),
     ('horizon/lib/bootstrap_datepicker',
-        xstatic.main.XStatic(xstatic.pkg.bootstrap_datepicker).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.bootstrap_datepicker,
+                             root_url=webroot).base_dir),
     ('bootstrap',
-        xstatic.main.XStatic(xstatic.pkg.bootstrap_scss).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.bootstrap_scss,
+                             root_url=webroot).base_dir),
     ('horizon/lib',
-        xstatic.main.XStatic(xstatic.pkg.d3).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.d3,
+                             root_url=webroot).base_dir),
     ('horizon/lib',
-        xstatic.main.XStatic(xstatic.pkg.hogan).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.hogan,
+                             root_url=webroot).base_dir),
     ('horizon/lib/font-awesome',
-        xstatic.main.XStatic(xstatic.pkg.font_awesome).base_dir),
-    ('horizon/lib/jasmine-1.3.1',
-        xstatic.main.XStatic(xstatic.pkg.jasmine).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.font_awesome,
+                             root_url=webroot).base_dir),
+    ('horizon/lib/jasmine',
+        xstatic.main.XStatic(xstatic.pkg.jasmine,
+                             root_url=webroot).base_dir),
     ('horizon/lib/jquery',
-        xstatic.main.XStatic(xstatic.pkg.jquery).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.jquery,
+                             root_url=webroot).base_dir),
     ('horizon/lib/jquery',
-        xstatic.main.XStatic(xstatic.pkg.jquery_migrate).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.jquery_migrate,
+                             root_url=webroot).base_dir),
     ('horizon/lib/jquery',
-        xstatic.main.XStatic(xstatic.pkg.jquery_quicksearch).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.jquery_quicksearch,
+                             root_url=webroot).base_dir),
     ('horizon/lib/jquery',
-        xstatic.main.XStatic(xstatic.pkg.jquery_tablesorter).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.jquery_tablesorter,
+                             root_url=webroot).base_dir),
     ('horizon/lib/jsencrypt',
-        xstatic.main.XStatic(xstatic.pkg.jsencrypt).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.jsencrypt,
+                             root_url=webroot).base_dir),
+    ('horizon/lib/magic_search',
+        xstatic.main.XStatic(xstatic.pkg.magic_search,
+                             root_url=webroot).base_dir),
     ('horizon/lib/qunit',
-        xstatic.main.XStatic(xstatic.pkg.qunit).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.qunit,
+                             root_url=webroot).base_dir),
     ('horizon/lib',
-        xstatic.main.XStatic(xstatic.pkg.rickshaw).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.rickshaw,
+                             root_url=webroot).base_dir),
     ('horizon/lib',
-        xstatic.main.XStatic(xstatic.pkg.spin).base_dir),
+        xstatic.main.XStatic(xstatic.pkg.spin,
+                             root_url=webroot).base_dir),
+    ('horizon/lib',
+        xstatic.main.XStatic(xstatic.pkg.termjs,
+                             root_url=webroot).base_dir),
     ('lib',
         xstatic.main.XStatic(xstatic.pkg.adminlte).base_dir),
     ('lib',
