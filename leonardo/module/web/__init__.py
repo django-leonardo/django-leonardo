b8d0132edc446cb49732913aa87d63e2d9c8b9fa
Michael Kutý <6du1ro.n@gmail.com>
2015-04-26 21:24:36 +0200

    added bootswatch to default supported themes

--- leonardo/module/web/__init__.py
+++ leonardo/module/web/__init__.py
@@ -26,7 +26,7 @@ class Default(object):
     def themes(self):
         """supported themes
         """
-        return ["leonardo_theme_adminlte", ]
+        return ['leonardo_theme_adminlte', 'leonardo_theme_bootswatch']
 
     @property
     def apps(self):
