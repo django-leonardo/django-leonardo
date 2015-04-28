4b0e112db8954dc589a46237c31ba24f81e6820c
Michael Kutý <6du1ro.n@gmail.com>
2015-04-28 14:39:46 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -12,4 +12,7 @@ except ImportError:
 setuptools.setup(
     setup_requires=['pbr'],
     pbr=True,
-    dependency_links=['https://github.com/michaelkuty/pbr/tarball/master#egg=pbr'])
+    dependency_links=[
+    	'https://github.com/michaelkuty/pbr/tarball/master#egg=pbr',
+		'https://github.com/michaelkuty/django-dbtemplates/tarball/develop#egg=dbtemplates'
+    ])
