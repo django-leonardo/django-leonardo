5dbf9497b7d515866bdf2ab2c85954e0ec75ab99
Ales Komarek <mail@newt.cz>
2015-05-01 09:45:33 +0200

    nav cleanups

--- leonardo/templatetags/leonardo_tags.py
+++ leonardo/templatetags/leonardo_tags.py
@@ -30,8 +30,8 @@ def feincms_render_region(context, feincms_object, region, request=None):
         if request and request.COOKIES.get(
                 'frontend_editing', False):
             url = reverse("widget_create", args=[feincms_object.id, region])
-            edit = """<a href="{0}" class='ajax-modal'>{1}</a>""".format(
+            edit = """<a href="{0}" class='btn btn-block btn-primary ajax-modal'>{1}</a>""".format(
                 url, _('Add'))
-    return edit + ''.join(
+    return ''.join(
         _render_content(content, request=request, context=context)
-        for content in getattr(feincms_object.content, region))
+        for content in getattr(feincms_object.content, region)) + edit
