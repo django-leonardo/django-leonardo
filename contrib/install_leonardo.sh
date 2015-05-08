319b2ca67b60dff5a02a1499417528d9b4c6c1f7
Michael Kutý <6du1ro.n@gmail.com>
2015-05-08 19:27:42 +0200

    script fixes

--- contrib/install_leonardo.sh
+++ contrib/install_leonardo.sh
@@ -4,13 +4,13 @@ virtualenv -p /usr/bin/python2.7 leonardo_venv
 cd leonardo_venv
 . $PWD/bin/activate
 
-pip install django-leonardo>=2015.0.4.dev17
+pip install django-leonardo==2015.0.4.dev17
 
 django-admin startproject --template=https://github.com/django-leonardo/site-template/archive/master.zip myproject
 
 export PYTHONPATH=$PWD/myproject
 echo $PYTHONPATH
-cd ./myproject
+cd myproject
 
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
