c1c5093f2b9c5817b8a23ab08cf68249ab76c30f
Michael Kutý <6du1ro.n@gmail.com>
2015-05-22 19:22:32 +0200

    path standard Django absolute url overrides behaviour

--- leonardo/apps.py
+++ leonardo/apps.py
@@ -11,6 +11,7 @@ class LeonardoConfig(AppConfig):
 
     def ready(self):
 
+        from django.conf import settings
         from django.template import defaulttags
 
         # monkey path url tag
@@ -31,7 +32,6 @@ class LeonardoConfig(AppConfig):
         try:
             # optionaly copy all live configuration to horizon/leonardo
             from constance import config
-            from django.conf import settings
 
             for k in dir(config):
                 if k not in HORIZON_CONFIG:
@@ -44,7 +44,6 @@ class LeonardoConfig(AppConfig):
         try:
             # optionaly copy all live configuration to main settings
             from constance import config
-            from django.conf import settings
 
             for k in dir(config):
                 setattr(settings, k, getattr(config, k))
@@ -60,3 +59,25 @@ class LeonardoConfig(AppConfig):
             debug.technical_404_response = technical_404_response
         except:
             pass
+
+        # now we patch all models with absolute url overrides
+        # this is important because Django path get_absolute_url when model
+        # is imported ! this behaviour breaks our strategy
+        from django.db.models.loading import get_model
+        from django.conf import settings
+        from django.utils.importlib import import_module  # noqa
+
+        for model, method in six.iteritems(settings.ABSOLUTE_URL_OVERRIDES):
+            try:
+                mod_name = model.split('.')[0]
+                model_cls =  get_model(mod_name, model.split('.')[-1])
+                if callable(method):
+                    _method = method
+                else:
+                    _mod = import_module(".".join(method.split('.')[:-1]))
+                    _method = getattr(_mod, method.split('.')[-1])
+
+                setattr(model_cls, 'get_absolute_url', _method)
+
+            except Exception as e:
+                raise e
