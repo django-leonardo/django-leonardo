9116c88b0b3d916b9335cc93e155c34db85428e5
Michael Kutý <6du1ro.n@gmail.com>
2015-05-22 09:52:15 +0200

    fail silently

--- leonardo/settings.py
+++ leonardo/settings.py
@@ -342,13 +342,15 @@ try:
             try:
                 _mod = import_module(".".join(method.split('.')[:-1]))
                 ABSOLUTE_URL_OVERRIDES[model] = getattr(_mod, method.split('.')[-1])
-            except Exception as e:
-                raise e
+            except:
+                # logging here
+                pass
 
         for nav_extension in mod_cfg.navigation_extensions:
             try:
                 import_module(nav_extension)
             except ImportError:
+                # logging here
                 pass
 
         CONSTANCE_CONFIG.update(mod_cfg.config)
