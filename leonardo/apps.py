c38501d68d356dc333157a7f5a89b2bcb1899ea8
Michael Kutý <6du1ro.n@gmail.com>
2015-06-08 01:21:47 +0200

    monkey patch compress behaviour

--- leonardo/apps.py
+++ leonardo/apps.py
@@ -81,3 +81,7 @@ class LeonardoConfig(AppConfig):
 
             except Exception as e:
                 raise e
+
+        # patch compress
+        from leonardo.utils.compress_patch import compress_monkey_patch
+        compress_monkey_patch()
