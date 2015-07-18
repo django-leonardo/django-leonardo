fa68a534c11f9cea8fdb2b22cd1270241dabd589
Ales Komarek <mail@newt.cz>
2015-07-18 13:25:08 +0200

    cleanups nav templates, simple link widget

--- leonardo/module/nav/models.py
+++ leonardo/module/nav/models.py
@@ -10,3 +10,25 @@ class NavigationWidget(Widget, NavigationWidgetMixin):
 
     class Meta:
         abstract = True
+
+
+class MediaCategoriesNavigationExtension(NavigationExtension):
+    name = _('')
+
+    def children(self, page, **kwargs):
+        base_url = page.get_absolute_url()
+        category_list = Folder.objects.filter(parent=None)
+        for category in category_list:
+            subchildren = []
+            for subcategory in category.media_folder_children.all():
+                subchildren.append(PagePretender(
+                    title=subcategory,
+                    url='%s%s/%s/' % (base_url, category.name, subcategory.name),
+                    level=5
+                ))
+            yield PagePretender(
+                title=category,
+                url='%s%s/' % (base_url, category.name),
+                children=subchildren,
+                level=5
+            )
