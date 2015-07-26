7f848e2a71971dc197d7f903d4f5d5dc6b4bfd60
Michael Kutý <6du1ro.n@gmail.com>
2015-07-27 00:34:27 +0200

    improve ContentProxyMixin and provide new JsonContentProxy

--- leonardo/module/web/widgets/mixins.py
+++ leonardo/module/web/widgets/mixins.py
@@ -2,8 +2,13 @@
 
 from __future__ import unicode_literals
 
-from django.utils import timezone
+import json
+from urlparse import urlparse
+
 from django.db import models
+from django.template.loader import render_to_string
+from django.utils import timezone
+from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
 from .const import PAGINATION_CHOICES
@@ -30,7 +35,11 @@ class ListWidgetMixin(models.Model):
 
 class ContentProxyWidgetMixin(models.Model):
 
-    """Content proxy widget mixin
+    """Provide basic fields and routines
+    for loading and caching data from external resource
+
+    define your implementation for getting data in ``get_data``
+    and use ``data`` property in your templates
     """
 
     source_address = models.CharField(
@@ -46,30 +55,74 @@ class ContentProxyWidgetMixin(models.Model):
         verbose_name=_('Cache update'),
         editable=False, null=True, blank=True)
 
+    @cached_property
+    def address_parser(self):
+        return urlparse(self.source_address)
+
+    @cached_property
+    def get_port(self):
+        """returns parsed port from ``source_address``
+        """
+        return self.address_parser.port
+
+    @cached_property
+    def get_host(self):
+        """returns parsed host from ``source_address``
+        """
+        return self.address_parser.hostname
+
     def is_obsolete(self):
         """returns True is data is obsolete and needs revalidation
         """
         if self.cache_updated:
             now = timezone.now()
             delta = now - self.cache_updated
-            if delta.seconds > self.cache_validity:
-                return True
-        else:
-            return True
-        return False
+            if delta.seconds < self.cache_validity:
+                return False
+        return True
 
     def update_cache(self, data=None):
-        """call after set data to self.cache_data or provide cached data
-        1. set your data to self.cache_data
-        2. call this method without saving the instance
+        """call with new data or set data to self.cache_data and call this
         """
         if data:
             self.cache_data = data
         self.cache_updated = timezone.now()
         self.save()
 
+    def get_data(self):
+        """define your behavior for loading raw data
+        """
+        raise NotImplementedError
+
+    @property
+    def data(self):
+        """this property just calls ``get_data``
+        but here you can serilalize your data or render as html
+        these data will be saved to self.cached_content
+        also will be accessable from template
+        """
+        if self.is_obsolete():
+            self.update_cache(self.get_data())
+        return self.cache_data
+
     class Meta:
         abstract = True
+
+
+class JSONContentMixin(object):
+
+    """just expect json data from ``get_data`` method
+    """
+
+    @property
+    def data(self):
+        """load and cache data in json format
+        """
+
+        if self.is_obsolete():
+            self.cache_data = json.dumps(self.get_data())
+            self.update_cache()
+        return json.loads(self.cache_data)
 
 
 class AuthContentProxyWidgetMixin(models.Model):
