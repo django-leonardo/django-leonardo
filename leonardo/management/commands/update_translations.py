f7fb6d1c1dc0bf2ec88470c065e10004a2e5c8db
Michael Kutý <6du1ro.n@gmail.com>
2015-09-06 11:59:08 +0200

    support for updating only specific applications

--- leonardo/management/commands/update_translations.py
+++ leonardo/management/commands/update_translations.py
@@ -13,16 +13,32 @@ class Command(BaseCommand):
 
     help = "Iterate over all application and generate-compile all translations"
     option_list = NoArgsCommand.option_list + (
-        make_option('--noinput',
-                    action='store_false', dest='interactive', default=True,
-                    help="Do NOT prompt the user for input of any kind."),
+        make_option('--apps',
+                    action='store_false', dest='interactive', default=[],
+                    help="Apps for updating as array delimeted with comma."),
     )
 
-    def handle(self, **options):
+    def handle(self, *args, **options):
 
         skipped_paths = []
 
-        for app_path in apps.get_app_paths():
+        if len(args) > 0:
+            app_names = args[0]
+            if not isinstance(app_names, list):
+                app_names = app_names.split(',')
+
+                _apps = []
+                for app_name in app_names:
+                    try:
+                        app = apps.get_app_config(app_name)
+                    except Exception as e:
+                        skipped_paths(app_name)
+                    else:
+                        _apps.append(app.path)
+        else:
+            _apps = apps.get_app_paths()
+
+        for app_path in _apps:
             try:
                 os.chdir(app_path)
                 call_command('makemessages')
