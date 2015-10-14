b517ed4c0d18559a45a6af548f1f569ec16a86a7
Michael Kutý <6du1ro.n@gmail.com>
2015-10-14 21:03:32 +0200

    remove profiling from default panels

--- leonardo/conf/debug.py
+++ leonardo/conf/debug.py
@@ -13,5 +13,4 @@ DEBUG_TOOLBAR_PANELS = [
     'debug_toolbar.panels.signals.SignalsPanel',
     'debug_toolbar.panels.logging.LoggingPanel',
     'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel'
 ]
