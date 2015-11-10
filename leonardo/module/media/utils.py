d79eadf8aec4c2b4c5255b430e1a118c84e21e54
Michael Kutý <6du1ro.n@gmail.com>
2015-11-10 11:16:33 +0100

    improve handle uploaded files

--- leonardo/module/media/utils.py
+++ leonardo/module/media/utils.py
@@ -3,13 +3,18 @@ from .models import Folder, MEDIA_MODELS
 
 
 def handle_uploaded_file(file, folder=None, is_public=True):
-    '''handle uploaded files to folder
+    '''handle uploaded file to folder
+    match first media type and create media object and returns it
 
-    match first media type and returns it
+    file: File object
+    folder: str or Folder isinstance
+    is_public: boolean
     '''
     _folder = None
 
-    if folder:
+    if folder and isinstance(folder, Folder):
+        _folder = folder
+    elif folder:
         _folder, folder_created = Folder.objects.get_or_create(
             name=folder)
 
@@ -26,3 +31,18 @@ def handle_uploaded_file(file, folder=None, is_public=True):
                 return obj
 
     return None
+
+
+def handle_uploaded_files(files, folder=None, is_public=True):
+    '''handle uploaded files to folder
+
+    files: array of File objects or single object
+    folder: str or Folder isinstance
+    is_public: boolean
+    '''
+    results = []
+
+    for f in files:
+        result = handle_uploaded_file(f, folder, is_public)
+        results.append(result)
+    return results
