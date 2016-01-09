237e3f917642432d476bf59b33dbe3185e4e2a53
Michael Kutý <6du1ro.n@gmail.com>
2016-01-09 21:05:00 +0100

    Properly render Page content in search templates.

--- leonardo/module/search/search_indexes.py
+++ leonardo/module/search/search_indexes.py
@@ -1,5 +1,7 @@
-from haystack import indexes, fields
+from django.test import RequestFactory
+from haystack import fields, indexes
 from leonardo.module.web.models import Page
+from leonardo.templatetags.leonardo_tags import _render_content
 
 
 class PageIndex(indexes.SearchIndex, indexes.Indexable):
@@ -20,10 +22,20 @@ class PageIndex(indexes.SearchIndex, indexes.Indexable):
     meta_description = fields.CharField(model_attr="meta_description")
     meta_keywords = fields.CharField(model_attr="meta_keywords")
     content_title = fields.CharField(model_attr="_content_title")
-    content = fields.CharField(model_attr="content")
+    content = fields.CharField()
 
     creation_date = fields.DateTimeField(model_attr='creation_date', null=True)
-    modification_date = fields.DateTimeField(model_attr='modification_date', null=True)
+    modification_date = fields.DateTimeField(
+        model_attr='modification_date', null=True)
+
+    def prepare_content(self, obj):
+        request_factory = RequestFactory()
+        request = request_factory.get(
+            obj.get_absolute_url(), data={'name': u'test'})
+        content = ''.join(
+            _render_content(content, request=request, context={})
+            for content in getattr(obj.content, 'col3'))
+        return content
 
     def should_update(self, instance, **kwargs):
         return instance.is_active()
@@ -35,5 +47,6 @@ class PageIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(active=True)
 
-    def get_updated_field(self):
-        return "modification_date"
+# I don't know if this date is changed if some widget was changed..
+#    def get_updated_field(self):
+#        return "modification_date"
