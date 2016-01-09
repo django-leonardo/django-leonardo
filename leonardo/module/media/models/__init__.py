828af6728ebd3bf498070672b691140c4713e769
Michael Kutý <6du1ro.n@gmail.com>
2016-01-09 19:34:55 +0100

    Tune search widget, simple image templates.

--- leonardo/module/media/models/__init__.py
+++ leonardo/module/media/models/__init__.py
@@ -5,5 +5,6 @@ from .filemodels import File  # noqa
 from .clipboardmodels import Clipboard, ClipboardItem  # noqa
 from .virtualitems import *
 from .media import *
+from . import tools
 
 MEDIA_MODELS = [Image, Document, Vector, Video, Flash, File]
