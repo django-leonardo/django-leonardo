e7a406b6c88d518f9b429598741977e10da5fc90
Michael Kutý <6du1ro.n@gmail.com>
2016-01-23 21:42:28 +0100

    Allow folder with spaces.

--- leonardo/module/media/apps/category_nested.py
+++ leonardo/module/media/apps/category_nested.py
@@ -3,7 +3,7 @@ from django.conf.urls import *
 urlpatterns = patterns('leonardo.module.media.views',
     (r'^$', 'directory_list_nested', {'directory_slug': None}),
     url(r'^fullscreen/(?P<category_id>[\w\-]+)/$', 'directory_detail_standalone', name="directory_detail_standalone"),
-    (r'^(?P<directory_slug>[\w\-]+)/$', 'directory_list_nested'),
+    (r'^(?P<directory_slug>.*)/$', 'directory_list_nested'),
     (r'^(?P<parent_directory_slug>[\w\-]+)/(?P<directory_slug>[\w\-]+)/$', 'directory_list_nested'),
     (r'^(?P<grandparent_directory_slug>[\w\-]+)/(?P<parent_directory_slug>[\w\-]+)/(?P<directory_slug>[\w\-]+)/$', 'directory_list_nested'),
 )
