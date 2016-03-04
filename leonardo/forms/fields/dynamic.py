1ab1e4e5d8a27eeaec03af47062f4961d0ac3599
Michael Kutý <6du1ro.n@gmail.com>
2016-03-04 16:03:40 +0100

    Cleanup.

--- leonardo/forms/fields/dynamic.py
+++ leonardo/forms/fields/dynamic.py
@@ -147,50 +147,47 @@ class DynamicSelectWidget(Select2Widget):
                 }
             })
               .done(function( data ) {
-                if ( console && console.log ) {
-                  console.log( "Sample of data:", data );
 
-                    horizon.modals._request = $.ajax(data.url, {
-                      beforeSend: function () {
-                        horizon.modals.modal_spinner(gettext("Loading"));
-                      },
-                      complete: function () {
-                        // Clear the global storage;
-                        horizon.modals._request = null;
-                        horizon.modals.spinner.modal('hide');
-                      },
-                      error: function(jqXHR, status, errorThrown) {
-                        if (jqXHR.status === 401){
-                          var redir_url = jqXHR.getResponseHeader("X-Horizon-Location");
-                          if (redir_url){
-                            location.href = redir_url;
-                          } else {
-                            location.reload(true);
-                          }
-                        }
-                        else {
-                          if (!horizon.ajax.get_messages(jqXHR)) {
-                            // Generic error handler. Really generic.
-                            horizon.alert("danger", gettext(
-                                "An error occurred. Please try again later."));
-                          }
-                        }
-                      },
-                      success: function (data, textStatus, jqXHR) {
-                        var update_field_id = 'data-add-to-field',
-                          modal,
-                          form;
-                        modal = horizon.modals.success(
-                            data, textStatus, jqXHR);
-                        if (update_field_id) {
-                          form = modal.find("form");
-                          if (form.length) {
-                            form.attr("data-add-to-field", update_field_id);
-                          }
-                        }
+                horizon.modals._request = $.ajax(data.url, {
+                  beforeSend: function () {
+                    horizon.modals.modal_spinner(gettext("Loading"));
+                  },
+                  complete: function () {
+                    // Clear the global storage;
+                    horizon.modals._request = null;
+                    horizon.modals.spinner.modal('hide');
+                  },
+                  error: function(jqXHR, status, errorThrown) {
+                    if (jqXHR.status === 401){
+                      var redir_url = jqXHR.getResponseHeader("X-Horizon-Location");
+                      if (redir_url){
+                        location.href = redir_url;
+                      } else {
+                        location.reload(true);
                       }
-                    });
-                }
+                    }
+                    else {
+                      if (!horizon.ajax.get_messages(jqXHR)) {
+                        // Generic error handler. Really generic.
+                        horizon.alert("danger", gettext(
+                            "An error occurred. Please try again later."));
+                      }
+                    }
+                  },
+                  success: function (data, textStatus, jqXHR) {
+                    var update_field_id = 'data-add-to-field',
+                      modal,
+                      form;
+                    modal = horizon.modals.success(
+                        data, textStatus, jqXHR);
+                    if (update_field_id) {
+                      form = modal.find("form");
+                      if (form.length) {
+                        form.attr("data-add-to-field", update_field_id);
+                      }
+                    }
+                  }
+                });
               });
         });
         </script>
