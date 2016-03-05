6162d821ab940bdee1594750ec09291b7fdfd622
Michael Kutý <6du1ro.n@gmail.com>
2016-03-05 14:06:28 +0100

    Patch whoosh rebuild index.

--- leonardo/module/search/tasks.py
+++ leonardo/module/search/tasks.py
@@ -1,12 +1,26 @@
 from __future__ import absolute_import
 
+import os
 from celery import shared_task
 from django.core import management
 from leonardo.decorators import catch_result
+from django.conf import settings
 
 
 @shared_task
 @catch_result
 def sync_search_indexes():
     management.call_command('rebuild_index', interactive=False)
+
+    # patch whoosh backend
+    haystack = getattr(settings, 'HAYSTACK_CONNECTIONS', None)
+
+    if 'default' in haystack and 'whoosh' in haystack['default']['ENGINE']:
+
+        try:
+            os.remove(os.path.join(
+                haystack['default']['PATH'], 'MAIN_WRITELOCK'))
+        except:
+            pass
+
     return {'result': 'Rebuild index OK'}
