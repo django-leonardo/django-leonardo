8a48c3bdcff7cd6761c25f1df3a7591dbb75854b
Michael Kutý <6du1ro.n@gmail.com>
2016-03-25 13:31:46 +0100

    Fix missing ungrouped widgets and include load widget classes in utils.

--- leonardo/utils/__init__.py
+++ leonardo/utils/__init__.py
@@ -1,9 +1,7 @@
 
-from leonardo.utils.settings import dotdict
-from leonardo.utils.settings import get_conf_from_module
-from leonardo.utils.settings import merge
+from leonardo.utils.settings import dotdict, get_conf_from_module, merge
 from leonardo.utils.widgets import (find_widget_class, get_all_widget_classes,
-                                    get_grouped_widgets)
+                                    get_grouped_widgets, load_widget_classes)
 
 __all__ = ('dotdict', 'get_conf_from_module', 'get_leonardo_modules',
            'merge', 'find_widget_class', 'get_all_widget_classes',
