4127677d6c3c9c09e803201e49d056edead7eec0
Michael Kutý <6du1ro.n@gmail.com>
2016-03-26 12:55:33 +0100

    Mark all edit views as staff member required.

--- leonardo/module/web/urls.py
+++ leonardo/module/web/urls.py
@@ -2,9 +2,9 @@
 from __future__ import absolute_import
 
 from django.conf.urls import include, patterns, url
-from horizon_contrib.forms.views import (CreateView,
-                                         UpdateView)
+from horizon_contrib.forms.views import CreateView, UpdateView
 from horizon_contrib.generic.views import GenericIndexView
+from leonardo.decorators import _decorate_urlconf, staff_member
 
 # override native horizon-contrib views
 GenericIndexView.template_name = "leonardo/common/_index.html"
@@ -16,3 +16,6 @@ urlpatterns = patterns('',
                        url(r'^page/', include('leonardo.module.web.page.urls')),
                        url(r'^widget/', include('leonardo.module.web.widgets.urls')),
                        )
+
+# mark all edit view as staff member required
+_decorate_urlconf(urlpatterns, staff_member)
