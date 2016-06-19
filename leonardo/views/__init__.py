4f47c7b74bfa8df1b9444c966b5437f0019fa494
Michael Kutý <6du1ro.n@gmail.com>
2016-06-19 21:15:42 +0200

    Mark all modals as standalone and fix base login.

--- leonardo/views/__init__.py
+++ leonardo/views/__init__.py
@@ -1,22 +1,6 @@
 from __future__ import absolute_import
 
-from horizon.tables.views import DataTableView  # noqa
-from horizon.tables.views import MixedDataTableView  # noqa
-from horizon.tables.views import MultiTableMixin  # noqa
-from horizon.tables.views import MultiTableView  # noqa
-from horizon_contrib.forms.views import (ContextMixin, CreateView,
-                                         ModalFormView, ModelFormMixin,
-                                         UpdateView)
-from leonardo.apps import standalone
-
-# set default template name
-ModalFormView.template_name = 'leonardo/common/modal.html'
-
-# mark all modal views as standalone
-ModalFormView.get = standalone(ModalFormView.get)
-UpdateView.get = standalone(UpdateView.get)
-CreateView.get = standalone(CreateView.get)
-
+from leonardo.forms.views import *
 
 __all__ = ('DataTableView', 'MixedDataTableView', 'MultiTableView',
            'MultiTableMixin', 'ContextMixin',
