f4c8124b6ed234d0d0f0b05a8e9743e0cda10ed6
Michael Kutý <6du1ro.n@gmail.com>
2016-09-26 13:50:05 +0200

    Add global permission helper and use permission denied instead custom not authorized.

--- leonardo/module/leonardo_auth/models.py
+++ leonardo/module/leonardo_auth/models.py
@@ -1,3 +1,16 @@
 
 from .widget.userlogin.models import UserLoginWidget
 from .widget.registration.models import UserRegistrationWidget
+from django.db import models
+
+
+class GlobalPermission(models.Model):
+
+    """This object is used to global permissions
+    """
+
+    class Meta:
+
+        # No database table creation or deletion operations \
+        # will be performed for this model.
+        managed = False
